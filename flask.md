2e43a6d399a4885896b4c24e8ebf395909be1b98
KateiRen <Karsten.Hartlieb@gmx.de>
2021-07-27 21:52:00 +0200

    template inheritance

--- flask.md
+++ flask.md
@@ -3,3 +3,9 @@
 in env "FLASK_DEBUG": "1" sorgt dafür, dass bei Änderungen der Server automatisch neu gestartet wird.
 
 
+Statische Inhalte in den Unterornder "static"
+
+Templates in den Unterordner "templates"
+
+render_template
+Die render-engine, die flask nutzt ist Jinja2, d.h. die Doku von Jinja2 ist relevant.
--- flask/hello.py
+++ flask/hello.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/test")
+def test():
+    args = request.args
+    name = args.get("name")
+    return render_template("test.html", name = name)
 
 @app.route("/template")
 def abcdefg():
